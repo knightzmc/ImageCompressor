e35f8f2a9d0b10a9579c682c9a3facfff91b9337
Alex Wood <alexljwood24@hotmail.co.uk>
2020-06-03 19:32:53 +0100

    Potential fix for SFTP uploading 7

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ fs.watch(directory, (_event, fileName) => {
         })
     }
 
-    compareSize(0, () => {
+    compareSize(-1, () => {
         compress(fileName, './build/', { statistic: true, autoupdate: true }, false, { jpg: { engine: 'mozjpeg', command: ['-quality', '60'] } }, { png: { engine: 'pngquant', command: ['--quality=20-50'] } }, { svg: { engine: 'svgo', command: '--multipass' } }, { gif: { engine: 'gifsicle', command: ['--colors', '64', '--use-col=web'] } },
             function(err, completed, statistic) {
                 logErr(err)
